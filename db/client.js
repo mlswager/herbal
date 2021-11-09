4bcff2d8d72f1d2df99f81fefe72cc28499c2089
Mason Swager <masonswager@masons-mbp.home>
2021-11-09 12:57:38 -0600

    working on deployment

--- db/client.js
+++ db/client.js
@@ -2,8 +2,18 @@ const {Client} = require("pg")
 
 const connectionString = process.env.DATABASE_URL || "https://localhost:5432/herbal"
 
+let config={}
+if (process.env.DATABASE_URL) {
+    config.dialectOptions = {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  }
+
+
 const client = new Client({
-    connectionString
+    connectionString,config
 })
 
 module.exports = client
