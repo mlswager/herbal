f2d3befa58f34dc32520c1a64189d3bc202dc5b7
Mason Swager <masonswager@masons-mbp.home>
2021-11-09 22:09:43 -0600

    still working on deployment3

--- db/client.js
+++ db/client.js
@@ -1,30 +1,28 @@
 const {Client} = require("pg")
 
-const connectionString = process.env.DATABASE_URL || "https://localhost:5432/herbal"
-
-const client = new Client({
-    connectionString,
-    ssl:
-    process.env.NODE_ENV === 'production'
-        ?{rejectUnauthorized: false}
-        :undefined,
-})
-
-// const CONNECTION_STRING = process.env.DATABASE_URL || "https://localhost:5432/herbal"
-
-// const config = {
-//     connectionString:CONNECTION_STRING
-// }
-
-// if(process.env.DATABASE_URL){
-//     config.ssl={
-//         rejectUnauthorized: false
-//     }
-// }
-
+// const connectionString = process.env.DATABASE_URL || "https://localhost:5432/herbal"
 
 // const client = new Client({
-//     config
+//     connectionString,
+//     ssl:
+//     process.env.NODE_ENV === 'production'
+//         ?{rejectUnauthorized: false}
+//         :undefined,
 // })
+
+const CONNECTION_STRING = process.env.DATABASE_URL || "https://localhost:5432/herbal"
+
+const config = {
+    connectionString:CONNECTION_STRING,
+}
+
+if(process.env.DATABASE_URL){
+    config.ssl={
+        rejectUnauthorized: false,
+    }
+}
+
+
+const client = new Client(config)
 
 module.exports = client
