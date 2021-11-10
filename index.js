02273a88677bc7e5fc8e87732c14d8eba68947ec
Mason Swager <masonswager@masons-mbp.home>
2021-11-10 09:47:02 -0600

    still working on deployment5

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const server = express()
 const morgan = require("morgan")
 const PORT = process.env.PORT||4000
+// const PORT = process.env.SERVER_PORT||4000
 
 
 server.use(morgan('dev'))
